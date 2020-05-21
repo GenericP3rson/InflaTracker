8ba5a3eeb8b8f36faa8345a4590e0fbdc8cfbb68
Saber Nomen <genericp3rson@gmail.com>
2020-05-21 09:48:39 -0500

    index.js works with one request; multipleRequests.js is still in the working

--- index.js
+++ index.js
@@ -3,45 +3,72 @@ const fs = require('fs');
 const sha1 = require('sha1');
 // const crypto = require('sha1');
 
-
 class DataBaseWork {
     /**
      * Sets up client. 
      * @param {String} data 
      */
-    constructor(data) {
-        this.client = new MongoClient(data); // Creates client
+    constructor(url) {
+        this.client = new MongoClient(url); // Creates client
     }
 
     /**
      * Hashes the password
      * @param {String} password 
      */
-    async hashPassword(password) {
+    hashPassword(password) {
         // console.log(hash.update(password).digest('base64'));
         return sha1(password);
     }
 
     /**
-     * Verifies whether the password is correct
-     * @param {MongoClient} client 
-     * @param {String} hash 
-     * @param {String} password 
+     * Gets a user from the DB
+     * @param {String} user 
      */
-    async allow(user, password) {
+    async getUser(user) {
+        let dat = null;
         try {
             await this.client.connect();
             let dbo = this.client.db("InflaTracker");
-            dbo.collection("InflaTracker").findOne({name: user}, (err, data) => {
+            dbo.collection("InflaTracker").findOne({ name: user }, (err, data) => {
                 if (err) throw err;
-                console.log(data);
-                return data && data.password == this.hashPassword(password);
+                dat = data;
             });
         } catch (err) {
             console.error(err);
         } finally {
             await this.client.close();
+            return dat;
         }
+    }
+
+    /**
+     * Verifies whether the user is in the DB
+     * @param {String} user 
+     */
+    userExists(user) {
+        let val = this.getUser(user).catch(console.error).then(token => { return token });
+        val.then((data) => {
+            console.log(data);
+            console.log(data!=null);
+            return data != null;
+        })
+    }
+
+    /**
+     * Verifies whether the password is correct
+     * @param {String} user 
+     * @param {String} password 
+     */
+    authenticate(user, password) {
+        let val = this.getUser(user).catch(console.error).then(token => { return token });
+        val.then((data) => {
+            if (data) {
+                console.log(data);
+                console.log(data.password == sha1(password));
+                return data.password == sha1(password);
+            } else console.error("USER DOES NOT EXIST");
+        })
     }
 
     /**
@@ -52,13 +79,37 @@ class DataBaseWork {
     async addUser(user, password) {
         try {
             await this.client.connect(); // Connects to client
+            // let val = this.getUser(user).catch(console.error).then(token => { return token });
+            // val.then((data) => {
+            //     if (!data) {
             let dbo = this.client.db("InflaTracker");
-            let pass = this.hashPassword(password);
-            console.log(pass + "");
-            let data = { name: user, password: pass };
+            let data = { name: user, password: this.hashPassword(password) };
             dbo.collection("InflaTracker").insertOne(data, function (err, res) {
                 if (err) throw err;
-                console.log("1 document inserted");
+                console.log("User added");
+            });
+                // } else console.error("USER DOES NOT EXIST");
+            // })
+        } catch (err) {
+            console.error(err);
+        } finally {
+            await this.client.close(); // Closes client connection
+        }
+    }
+
+    async changeCredentials(oldUser, newUser, newPass) {
+        try {
+            await this.client.connect(); // Connects to client
+            // let val = this.getUser(user).catch(console.error).then(token => { return token });
+            // val.then((data) => {
+            //     if (!data) {
+            let dbo = this.client.db("InflaTracker");
+            let myquery = { name: oldUser };
+            let newvalues = { $set: { name: newUser, password: this.hashPassword(newPass) } };
+            dbo.collection("InflaTracker").updateOne(myquery, newvalues, function (err, res) {
+                if (err) throw err;
+                console.log("1 user updated");
+                // db.close();
             });
         } catch (err) {
             console.error(err);
@@ -69,12 +120,13 @@ class DataBaseWork {
 
 }
 
+
 fs.readFile('clienturl.txt', 'utf8', (err, data) => {
     if (err) {
         return console.error(err);
     } else {
         const url = data; // For safety's sake, our user credentials is on a separate doc
-        let dbwork = new DataBaseWork(url);;
-        dbwork.allow("Name1", "Password").catch(console.error);
+        let dbwork = new DataBaseWork(url);
+        console.log(dbwork.changeCredentials("Name2", "Name2", "Hello"));
     }
 });
